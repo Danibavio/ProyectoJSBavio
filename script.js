77f98b9e80889e22e36414971b0c529b35e3f91e
Danibavio <danielbavio1@gmail.com>
2022-10-10 19:36:23 -0300

    Agregando arrays y objetos

--- script.js
+++ script.js
@@ -1,58 +1,48 @@
 alert("Bienvenide a Fulvito")
 
-let camisetaRiver = "River"
-let precioCamisetaRiver = 1000
-let stockCamisetaRiver = 9
+class camisetas {
+    constructor(equipo, precio) {
+        this.equipo = equipo;
+        this.precio = precio;
+    }
+}
 
-let camisetaBoca = "Boca"
-let precioCamisetaBoca = 900
-let stockCamisetaBoca = 12
+let camisetaRiver = new camisetas("River Plate", 1000)
+let camisetaBocaJrs = new camisetas("Boca Juniors", 900)
+let camisetaArgentina = new camisetas("Argentina", 2000)
 
-let camisetaArg = "Selección Argentina"
-let precioCamisetaArg = 2000
-let stockCamisetaArg = 18
+let listaCamisetas = [camisetaRiver, camisetaBocaJrs, camisetaArgentina]
 
+console.log(listaCamisetas)
+let carrito =[]
 let precioTotal = 0
 let iva = 0.21
-alert("Tenemos camisetas de los siguientes equipos: \n - River: $1000 stock: 9 \n - Boca: $900 stock: 12\n - Seleccion Argentina: $2000 stock: 18" )
+
+let listaEquipos = listaCamisetas.map((camisetas) => camisetas.equipo + " " + "$" + camisetas.precio)
+
+alert("Estos son nuestros productos: \n - " + listaEquipos.join("\n - "))
 
 let elegirCamiseta = prompt("Ingrese de que equipo quiere comprar o ingrese 'LISTO' para finalizar la compra.")
 
-while(elegirCamiseta.toUpperCase() != "LISTO"){
-    if (elegirCamiseta.toLowerCase() == "river"){
-        let cantidadCamisetaRiver = prompt("Ingrese que cantidad de " + camisetaRiver + " desea comprar ")
-        if (cantidadCamisetaRiver <= stockCamisetaRiver){
-            prompt ("Ingrese el talle de camiseta que quiera \n -M \n -L \n -XL")
-            precioTotal = precioTotal + (cantidadCamisetaRiver * precioCamisetaRiver)
-        }
-        else{
-            alert("No contamos con el suficiente stock :(" )
-        }
+while(elegirCamiseta != "LISTO"){
+    
+    if (elegirCamiseta == "River"){
+        let cantidadCamisetaRiver = prompt("Ingrese que cantidad de " + camisetaRiver.equipo + " desea comprar ")
+            precioTotal = precioTotal + (cantidadCamisetaRiver * camisetaRiver.precio)
     }
-    else if (elegirCamiseta.toLowerCase() == "Boca"){
-        let cantidadCamisetaBoca = prompt("Ingrese que cantidad de " + camisetaBoca + " desea comprar ")
-        if (cantidadCamisetaBoca <= stockCamisetaBoca){
-            prompt ("Ingrese el talle de camiseta que quiera \n -M \n -L \n -XL")
-            precioTotal = precioTotal + (cantidadCamisetaBoca * precioCamisetaBoca)
-        }
-        else{
-            alert("No contamos con el suficiente stock :(" )
-        }
+    else if (elegirCamiseta == "Boca"){
+        let cantidadCamisetaBoca = prompt("Ingrese que cantidad de " + camisetaBocaJrs.equipo + " desea comprar ")
+            precioTotal = precioTotal + (cantidadCamisetaBoca * camisetaBocaJrs.precio)
+
     }
-    else if (elegirCamiseta.toLowerCase() == "Argentina", "Seleccion", "arg"){
-        let cantidadCamisetaArg = prompt("Ingrese que cantidad de " + camisetaArg + " desea comprar ")
-        if (cantidadCamisetaArg <= stockCamisetaArg){
-            prompt ("Ingrese el talle de camiseta que quiera \n -M \n -L \n -XL")
-            precioTotal = precioTotal + (cantidadCamisetaArg * precioCamisetaArg)
-        }
-        else{
-            alert("No contamos con el suficiente stock :(" )
-        }
+    else if (elegirCamiseta == "Argentina" || "Seleccion" || "arg"){
+        let cantidadCamisetaArg = prompt("Ingrese que cantidad de " + camisetaArgentina.equipo + " desea comprar ")
+            precioTotal = precioTotal + (cantidadCamisetaArg * camisetaArgentina.precio)
     }
     else{
-        alert("No contamos con camisetas de ese equipo :(")
+        alert("No contamos con ese producto")
     }
-    elegirCamiseta = prompt ("¿Queres la camiseta de algun otro equipo? \n Ingrese de que equipo quiere comprar o ingrese 'LISTO' para salir. \n - River: $1000 \n - Boca: $900 \n - Seleccion Argentina: $2000 ")
+    elegirCamiseta = prompt ("¿Queres la camiseta de algun otro equipo? \n Ingrese de que equipo quiere comprar o ingrese 'LISTO' para salir \n - " + listaEquipos.join("\n -"))
 }
 function calcularIVA(a, b){
     return a * b;
@@ -61,6 +51,8 @@ let resultadoFinal = calcularIVA(precioTotal, iva)
 if (precioTotal != 0){
     alert ("el precio total con IVA incluido es: " + (precioTotal + resultadoFinal) + " pesos")
     }
+alert("Gracias por tu compra")
     
+
 
 
